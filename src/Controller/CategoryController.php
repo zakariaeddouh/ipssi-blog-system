847b9d84d0034f9d1426c305deff4e99d9364100
Zakaria Eddouh <zakaria.eddouh@gmail.com>
2026-02-11 17:28:45 +0100

    form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,6 +20,26 @@ final class CategoryController extends AbstractController
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
             'categories' => $categories
+        ]);
+    }
+
+    #[Route('/category/new', name: 'app_category_new')]
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ) : Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+
+         return $this->render('category/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
